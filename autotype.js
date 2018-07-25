87fad6da0c909e8a6b2fd8cd2918640e6c2ad21a
eyevent <eyevent@USSEAGirls01.ey.net>
2018-07-25 14:01:48 -0700

    first commit

---
+++ autotype.js
@@ -0,0 +1,9 @@
+var text = "The FitnessGram Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal. [beep] A single lap should be completed each time you hear this sound. [ding] Remember to run in a straight line, and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark, get ready, start."
+var i = 0;
+
+function addChar() {
+  var charToAdd = document.createTextNode(text.charAt(i));
+  var textenter = document.getElementById("textenter");
+  textenter.appendChild(charToAdd);
+  i++;
+}
